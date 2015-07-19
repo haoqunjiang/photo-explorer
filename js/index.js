08a8fb0f7b52cee2117b33aa6534ba80adf9c7e4
sodatea <sodazju@gmail.com>
2015-07-20 03:21:44 +0800

    基本的浏览大图功能

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 // TODO
-// 目前代码耦合太紧密，不利于单元测试，须想办法解耦
+// 1. 浏览大图时左右箭头位置应当固定
+// 2. 消除浏览大图时的闪烁
+// 3. 目前代码耦合太紧密，不利于单元测试，须想办法解耦
 
-var app = {
+var waterfall = {
     totalColumns: 0,
     columnOffsets: [],
 
@@ -10,12 +12,16 @@ var app = {
     containerSelector: '.main',
 
     init: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
         // 设置 content 区域宽度要在 DOMReady 时做，避免闪烁
         // 事实上还是会有轻微闪烁，因为当第二次刷新页面时，图片已有缓存，在宽度设置好之前就已经显示出来了
         $(this.setContentWidth.bind(this));
 
         // 图片全部载入完成后进行重排
-        $(window).on('load', this.waterfall.bind(this));
+        $(window).on('load', this.reflow.bind(this));
 
         // 滚动到页面底部后触发「加载更多」事件
         $(window).on('scroll', this.onScrollHandler.bind(this));
@@ -23,7 +29,7 @@ var app = {
         var self = this;
         $(window).on('resize', function() {
             self.setContentWidth();
-            self.waterfall();
+            self.reflow();
         });
     },
 
@@ -39,7 +45,7 @@ var app = {
     },
 
     // 将页面上的图片按瀑布流的方式重排
-    waterfall: function() {
+    reflow: function() {
         var $items = $(this.itemSelector);
 
         this.columnOffsets = []; // 需要重新初始化为空数组，因为如果页面 resize 了列数可能变化
@@ -48,7 +54,7 @@ var app = {
         for (var i = 0; i !== this.totalColumns; ++i) {
             var $item = $items.eq(i);
 
-            $item.css('position', 'static');    // 考虑到页面 resize 的情况，需重置 CSS
+            $item.css('position', 'static');    // 考虑到页面 resize 的情况，须重置样式
 
             this.columnOffsets[i] = {
                 top: 0,
@@ -134,4 +140,89 @@ var app = {
     }
 };
 
-app.init();
+var gallery = {
+    $item: null,
+
+    init: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        $('body').on('click', '.pic-item img', this.showGallery.bind(this));
+        $('body').on('click', '.gallery-next', this.nextPicture.bind(this));
+        $('body').on('click', '.gallery-prev', this.prevPicture.bind(this));
+        $('body').on('click', '.gallery-close', this.closeGallery.bind(this));
+        $('body').on('click', '.overlay', this.closeGallery.bind(this));
+    },
+
+    // 显示图片浏览器
+    showGallery: function(evt) {
+        this.$item = $(evt.target).parent();
+        this.showPicture($(evt.target).data('large-src'));
+
+        $('.overlay, .gallery').removeClass('hidden');
+    },
+
+    // 关闭图片浏览
+    closeGallery: function() {
+        $('.gallery, .overlay').addClass('hidden');
+    },
+
+    // 下一张
+    nextPicture: function() {
+        this.$item = this.$item.next();
+        this.showPicture(this.$item.find('img').data('large-src'));
+    },
+
+    // 上一张
+    prevPicture: function() {
+        this.$item = this.$item.prev();
+        this.showPicture(this.$item.find('img').data('large-src'));
+    },
+
+    showPicture: function(src) {
+        if (!src) {
+            return;
+        }
+
+        var img = new Image();
+        img.src = src;
+        img.onload = function() {
+            var w = this.width;
+            var h = this.height;
+            var winHeight = $(window).height();
+            var winWidth = $(window).width();
+
+            this.style.width = 'auto';
+            this.style.height = 'auto';
+
+            if (w / h > 1) {
+                // 如果是横向的图，最大宽度为 窗口宽度 - 120
+                // 左右两侧留 50px 用于导航，此外上下需要稍微有点空隙不能顶着边缘，所以 +20px
+                // 这里假设窗口一定宽于 100px（窄于 100px 的根本没有任何兼容必要嘛……）
+                this.style.maxWidth = (winWidth - 150) + 'px';
+            } else {
+                // 纵向图的高度等于窗口高度
+                this.style.maxHeight = winHeight + 'px';
+            }
+        };
+
+        $('.gallery-picture').empty().append(img);
+        this.showArrows();
+    },
+
+    showArrows: function() {
+        // 显示所有箭头
+        $('.gallery-nav').removeClass('hidden');
+
+        // 如果是最后一张图，隐藏「下一张」；如果是第一张图，隐藏「上一张」
+        if (this.$item.is(':last-child')) {
+            $('.gallery-next').addClass('hidden');
+        } else if (this.$item.is(':first-child')) {
+            $('.gallery-prev').addClass('hidden');
+        }
+    }
+};
+
+waterfall.init();
+gallery.init();
